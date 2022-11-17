0a6368d2dce23094d57dc897d01ee5d16038a769
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-17 16:07:57 -0300

    feat: create functions that handle with read more and favorite button

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
@@ -7,12 +8,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  @Input() dataBook!:any;
+  @Input() dataBook!: any;
+
+  showOnlyShortDescription = "";
+  shouldOpenReadMore: boolean = false;
+  favoriteBook = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.dataBook)
+    if(this.dataBook.volumeInfo.description && this.dataBook.volumeInfo?.description.split(" ").length > 20){
+      this.showOnlyShortDescription = this.dataBook.volumeInfo.description.split(" ").slice(0,20).join(" ");
+    }else{
+      this.toogleReadMore();
+    }
+  }
+
+  toogleReadMore(){
+    this.shouldOpenReadMore = !this.shouldOpenReadMore;
+  }
+
+  toogleFavoriteButton(){
+    this.favoriteBook = !this.favoriteBook;
   }
 
 }
