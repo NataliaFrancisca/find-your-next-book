cfa23b078b7dae15721f92a56354ff6dce6440f7
Natalia Francisca <nathifrancisca@gmail.com>
2023-05-26 21:37:39 -0300

    refactor: add static typing to the fetch function

--- src/app/service/fetch.service.ts
+++ src/app/service/fetch.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BookResponse } from 'src/types/types';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,11 @@ export class FetchService {
 
   constructor() { }
 
-  fetchBook(book: string): any{
-    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`)
-      .then(res => res.json())
-      .then(data => data.items)
-      .catch(error => console.error(error))
+  async fetchBook(searchBook: string): Promise<BookResponse> {
+    const resultAPI = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchBook}`)
+    const dataJSON = await resultAPI.json();
+    return dataJSON.items;
   }
+
 }
+
