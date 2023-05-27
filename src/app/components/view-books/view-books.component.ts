d3a62697a5c6f99c5b0b29af1305fde6c8ed9a8a
Natalia Francisca <nathifrancisca@gmail.com>
2023-05-26 21:41:36 -0300

    feat: add the type BookResponse to the @Input

--- src/app/components/view-books/view-books.component.ts
+++ src/app/components/view-books/view-books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { BookResponse } from 'src/types/types';
 
 @Component({
   selector: 'app-view-books',
@@ -6,7 +7,7 @@ import { Component, OnInit, Input} from '@angular/core';
   styleUrls: ['./view-books.component.css']
 })
 export class ViewBooksComponent implements OnInit {
-  @Input() dataBooks: Array<any> = [];
+  @Input() bookResponseData: Array<BookResponse> = [];
   @Input() titleView!:string;
 
   constructor() { }
