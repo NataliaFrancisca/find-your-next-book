091d9d30feaa3b356f19a19653cabf179985c978
Natalia Francisca <nathifrancisca@gmail.com>
2023-05-26 21:39:05 -0300

    refactor: add types to the function; variables and rename the functions

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,43 +1,54 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CrudService } from 'src/app/service/crud.service';
+import { BookResponse, BookVolumeInfo } from 'src/types/types';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit{
+  @Input() bookResponse!: BookResponse;
 
-  @Input() dataBook!: any;
-
-  showOnlyShortDescription = "";
-  shouldOpenReadMore: boolean = false;
+  volumeBookData!: BookVolumeInfo;
+  
   favoriteBook = false;
+  stateToggleReadMore: boolean = false;
 
-  constructor(private crudService: CrudService) { }
+  shortBookDescription: string = '';
+  bookImage = 'assets/images/image-not-found.png';
+
+  constructor(private crudService: CrudService) {}
 
   ngOnInit(): void {
-    if(this.dataBook.volumeInfo.description && this.dataBook.volumeInfo?.description.split(" ").length > 20){
-      this.showOnlyShortDescription = this.dataBook.volumeInfo.description.split(" ").slice(0,20).join(" ");
-    }else{
-      this.toogleReadMore();
+    this.volumeBookData = this.bookResponse.volumeInfo;
+    this.shortBookDescription = this.volumeBookData.description?.split(" ").slice(0, 20).join(" ");
+
+    if(this.volumeBookData.hasOwnProperty('imageLinks')){
+      this.bookImage = this.volumeBookData.imageLinks.thumbnail;
     }
 
     this.isFavoriteBook();
   }
 
-  toogleReadMore(){
-    this.shouldOpenReadMore = !this.shouldOpenReadMore;
+  // Buttons Toogle
+  buttonToogleReadMore(){
+    this.stateToggleReadMore = !this.stateToggleReadMore;
   }
 
-  toogleFavoriteButton(){
+  buttonToogleFavorite(): void{
     this.favoriteBook = !this.favoriteBook;
-    this.favoriteBook ? this.crudService.saveBook(this.dataBook) : this.crudService.deleteBook(this.dataBook);
+    this.updateFavoriteBooks();
+  }
+
+  // Services from CRUD
+  updateFavoriteBooks(): void{
+    this.favoriteBook ? this.crudService.saveBook(this.bookResponse) : this.crudService.deleteBook(this.bookResponse);
   }
 
   isFavoriteBook(){
-    const booksFavorite = this.crudService.getBooks();
-    const isFavorite = booksFavorite.some((booksFavorite: any) => booksFavorite.id === this.dataBook.id)
+    const listBooks = this.crudService.getBooks();
+    const isFavorite = listBooks.some((listBooks: BookResponse) => listBooks.id === this.bookResponse.id)
     this.favoriteBook = isFavorite;
   }
 }
