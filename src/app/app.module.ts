9f880df4bdd94ddca7f6370a6bde8cc5e56c33e9
Natalia Francisca <nathifrancisca@gmail.com>
2023-06-01 22:33:14 -0300

    feat: import new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { ViewBooksComponent } from './components/view-books/view-books.component';
 import { BookComponent } from './components/book/book.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     ViewBooksComponent,
-    BookComponent
+    BookComponent,
+    HomePageComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
