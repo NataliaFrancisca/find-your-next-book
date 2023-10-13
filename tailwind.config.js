b3d2fa517ecf5a3bacb14b9b5d9b973a043ec127
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-12 23:45:52 -0300

    feat(style): add new colors; add the border width

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-green': 'rgba(57, 125, 119, 0.24)',
-        'light-green': 'rgba(93, 166, 150, 0.05)',
+        'base-black': '#202020',
+        'primary': '#94C0E9',
+        'background': '#E1EDFF'
       },
       boxShadow: {
         'default': '0px 4px 4px rgba(0, 0, 0, 0.25)'
+      },
+      borderWidth: {
+        '3': '3px'
       }
       
     },
