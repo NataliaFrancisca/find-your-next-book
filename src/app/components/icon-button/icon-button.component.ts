2c9b9616c6edb7fe0df6dc20c01b1d14c46bd983
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-16 22:40:36 -0300

    feat: create the component that represent the icon button

---
+++ src/app/components/icon-button/icon-button.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+type TObjectKey = {
+  [key: string]: {
+    default: string,
+    checked: string
+  }
+}
+
+@Component({
+  selector: 'app-icon-button',
+  templateUrl: './icon-button.component.html',
+  styleUrls: ['./icon-button.component.css']
+})
+export class IconButtonComponent implements OnInit {
+  @Output() buttonEvent: EventEmitter<boolean> = new EventEmitter;
+  @Input() buttonStatus!: boolean;
+  @Input() buttonName!: string;
+
+  src_icon: TObjectKey = {
+    favorite: {
+      default: 'favorite.svg',
+      checked: 'favorite-checked.svg',
+    },
+    saved: {
+      default: 'saved.svg',
+      checked: 'saved-checked.svg',
+    },
+    finished: {
+      default: 'finished.svg',
+      checked: 'finished-checked.svg',
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onToggle(){
+    this.buttonEvent.emit();
+  }
+
+}
