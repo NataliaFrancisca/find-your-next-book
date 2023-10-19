84598e15aabe5a7eb6983f2c805f8264eb1b2cbd
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-18 21:41:13 -0300

    feat: create the crud functions

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { IResponseAPI } from 'src/types/types';
+
+type OBJ = {
+  favorite: Array<IResponseAPI>,
+  saved: Array<IResponseAPI>,
+  finished: Array<IResponseAPI>
+}
 
 @Injectable({
   providedIn: 'root'
@@ -7,32 +14,88 @@ export class CrudService {
 
   constructor() {
     const storage = localStorage.getItem("books") ||  "";
+    
     if(storage == ""){
-      localStorage.setItem("books", JSON.stringify([]));
+      localStorage.setItem("books", JSON.stringify({favorite: [], saved: [], finished: []}));
     }
   }
 
-  checkAlreadyAddBook(book: any, books: Array<any>){
-    return books.some(bookStorage => bookStorage.id === book.id)
+  checkDuplicatedBook(book: IResponseAPI, response: Array<IResponseAPI>){
+    return response.some(bookResponse => bookResponse.id == book.id)
   }
 
-  saveBook(book: Object){
-    const storage = localStorage.getItem("books") ||  "";
+  getJSON(){
+    const storage = localStorage.getItem('books') || '';
+    return JSON.parse(storage) as OBJ;
+  }
 
-    if(!this.checkAlreadyAddBook(book, JSON.parse(storage))){
-      const updatedValues = [...JSON.parse(storage), book];
-      localStorage.setItem("books", JSON.stringify(updatedValues))
+  setFavorite(book: IResponseAPI){
+    const obj = this.getJSON();
+    const status_duplicates = this.checkDuplicatedBook(book, obj.favorite);
+
+    if(!status_duplicates){
+      obj.favorite = [...obj.favorite, book];
+      localStorage.setItem('books', JSON.stringify(obj));
     }
   }
 
-  getBooks(){
-    const storage = localStorage.getItem("books") || "You don't have books saved";
-    return JSON.parse(storage);
+  setFinished(book: IResponseAPI){
+    const obj = this.getJSON();
+    const status_duplicates = this.checkDuplicatedBook(book, obj.finished);
+
+    if(!status_duplicates){
+      obj.finished = [...obj.finished, book];
+      localStorage.setItem('books', JSON.stringify(obj));
+    }
   }
 
-  deleteBook(book: any){
-    const storage = localStorage.getItem("books") || "You don't have books saved";
-    const updateStorage = JSON.parse(storage).filter((bookStorage: any) => bookStorage.id !== book.id)
-    localStorage.setItem("books", JSON.stringify(updateStorage))
+  setSaved(book: IResponseAPI){
+    const obj = this.getJSON();
+    const status_duplicates = this.checkDuplicatedBook(book, obj.saved);
+
+    if(!status_duplicates){
+      obj.saved = [...obj.saved, book];
+      localStorage.setItem('books', JSON.stringify(obj));
+    }
+  }
+
+  getBooks(filter: string){
+    const storage = this.getJSON();
+
+    switch(filter){
+      case 'favorite':
+        return storage.favorite;
+      case 'finished':
+        return storage.finished;
+      case 'saved':
+        return storage.saved;
+      default:
+        return [];
+    }
+  }
+
+  deleteBook(id: number, filter: string){
+    const storage = this.getJSON();
+ 
+    if(filter == 'favorite'){
+      const updated_storage = storage.favorite.filter((bookStorage: IResponseAPI) => bookStorage.id !== id);
+      storage.favorite = updated_storage;
+      localStorage.setItem('books', JSON.stringify(storage));
+      return;
+    }
+
+    if(filter == 'finished'){
+      const updated_storage = storage.finished.filter((bookStorage: IResponseAPI) => bookStorage.id !== id);
+      storage.finished = updated_storage;
+      localStorage.setItem('books', JSON.stringify(storage));
+      return;
+    }
+
+    if(filter == 'saved'){
+      const updated_storage = storage.saved.filter((bookStorage: IResponseAPI) => bookStorage.id !== id);
+      storage.saved = updated_storage;
+      localStorage.setItem('books', JSON.stringify(storage));
+      return;
+    }
   }
 }
