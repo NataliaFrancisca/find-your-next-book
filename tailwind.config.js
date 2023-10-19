f89b0ec7458ecbbc40690af455a29ab556a61dc9
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-19 20:02:40 -0300

    feat: add londrina font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'londrina-solid': ['Londrina Solid', 'sans-serif'],
+        'londrina-sketch': ['Londrina Sketch', 'sans-serif'],
+      },
       colors: {
         'base-black': '#202020',
-        'primary': '#64A100',
-        'background': '#f0ffff'
+        'primary': '#5738AF',
+        'secondary': '#D0D5FD',
+        'background': '#F7F4EF'
       },
       boxShadow: {
         'default': '0px 4px 4px rgba(0, 0, 0, 0.25)'
@@ -19,7 +24,6 @@ module.exports = {
       borderRadius: {
         '3': '3px'
       }
-      
     },
   },
   plugins: [],
