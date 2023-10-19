c5ad2e7c0ac99d3eb2bd372a3e557ab65180cbd2
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-18 21:34:29 -0300

    feat: remove code that I was not using

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CrudService } from './service/crud.service';
 
 @Component({
   selector: 'app-root',
@@ -7,32 +6,5 @@ import { CrudService } from './service/crud.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-angular';
-  titleViewPage = "Resultados";
-
-  stateToggleViewBooks = false;
-
-  shouldShowSavedBooks: boolean = false;
-  bookData = [];
-
-  constructor(private crudService: CrudService){}
-
-  onUpdateData(data: any){
-    this.bookData = data;
-    this.titleViewPage = "Results:"
-  }
-
-  buttonToogleViewBooks(){
-    this.stateToggleViewBooks = !this.stateToggleViewBooks;
-
-    if(this.stateToggleViewBooks){
-      const books = this.crudService.getBooks();
-      this.bookData = books;
-      this.titleViewPage = "My library:"
-    }else{
-      this.bookData = [];
-      this.titleViewPage = "Results:"
-    }
-
-  }
+  constructor(){}
 }
