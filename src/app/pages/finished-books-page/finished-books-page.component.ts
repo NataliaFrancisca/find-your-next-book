02e63ad2c61be7df47408adc1fce431ada9ba2d9
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-19 23:13:46 -0300

    feat: cretae the page that shows the finished books

--- src/app/pages/finished-books-page/finished-books-page.component.ts
+++ src/app/pages/finished-books-page/finished-books-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+import { IResponseAPI } from 'src/types/types';
 
 @Component({
   selector: 'app-finished-books-page',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finished-books-page.component.css']
 })
 export class FinishedBooksPageComponent implements OnInit {
-
-  constructor() { }
+  bookResult!: Array<IResponseAPI>;
+  loader_status = false;
+  
+  constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
+    this.onFetchData();
+  }
+
+  onUpdateLoaderStatus(status: boolean){
+    this.loader_status = status;
+  }
+
+  onFetchData(){
+    this.loader_status = true;
+    const result = this.crudService.getBooks('finished');
+    this.bookResult = result;
+    this.loader_status = false;
   }
 
 }
