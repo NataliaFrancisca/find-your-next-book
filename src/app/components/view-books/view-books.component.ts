e462f15e9718643a64baf03b5520701186d2ef63
Natalia Francisca <nathifrancisca@gmail.com>
2023-10-19 23:12:42 -0300

    feat: use the function to update the view

--- src/app/components/view-books/view-books.component.ts
+++ src/app/components/view-books/view-books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { IResponseAPI } from 'src/types/types';
 
 @Component({
@@ -8,11 +8,16 @@ import { IResponseAPI } from 'src/types/types';
 })
 export class ViewBooksComponent implements OnInit {
   @Input() bookResult: Array<IResponseAPI> = [];
-  @Input() viewName!: String;
-  
+  @Input() viewName!: string;
+  @Output() updateView: EventEmitter<boolean> = new EventEmitter;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onUpdateCurrentView(){
+    this.updateView.emit();
   }
 
 }
